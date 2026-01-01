22c94182d088decaf62054d4d28e9ff8b479b758
Shriprasad DJ <shriprasaddj@Shriprasads-MacBook-Air-2.local>
2026-01-01 12:13:34 +0530

    Fix CORS for frontend access

--- app/main.py
+++ app/main.py
@@ -2,13 +2,31 @@ from fastapi import FastAPI
 from app.database import Base, engine
 from app.routes import dishes
 
+from fastapi.middleware.cors import CORSMiddleware
+
+
 # Create tables (safe for Neon)
 Base.metadata.create_all(bind=engine)
+
+
 
 app = FastAPI(
     title="Foodi API",
     version="1.0.0"
 )
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",       # local dev
+        "https://foddiebackendapp.vercel.app",  # prod frontend (later)
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(dishes.router)
 
 app.include_router(dishes.router)
 
