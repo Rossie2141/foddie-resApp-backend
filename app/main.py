18cf1b1668508f68c5b68a273731abc04e98be21
Shriprasad DJ <shriprasaddj@Mac.bbrouter>
2026-01-02 12:31:35 +0530

    main 1

--- app/main.py
+++ app/main.py
@@ -19,7 +19,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",       # local dev
-        "https://foddiebackendapp.vercel.app",  # prod frontend (later)
+        # "https://foddiebackendapp.vercel.app",  # prod frontend (later)
     ],
     allow_credentials=True,
     allow_methods=["*"],
